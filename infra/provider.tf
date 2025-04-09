f94cc94979ea0770abccb48c16e135d3bf75c0a2
Denis Bulgakov <praeitor@gmail.com>
2025-04-09 09:24:11 +0400

    changes in terraform configs

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,19 +6,20 @@ terraform {
   }
   required_version = ">= 0.13"
 
-  backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-    bucket = "common-bucket"
-    region = "ru-central1"
-    key    = "tf-state.tfstate"
+  #backend "s3" {
+  #  endpoints = {
+  #    s3 = "https://storage.yandexcloud.net"
+  #  }
+  #  bucket = "kittygram-tf-state"
+  #  region = "ru-central1"
+  #  key    = "tf-state.tfstate"
+  #  use_path_style = true
 
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-  }
+  #  skip_region_validation      = true
+  #  skip_credentials_validation = true
+  #  skip_requesting_account_id  = true
+  #  skip_s3_checksum            = true
+  #}
 }
 
 provider "yandex" {
