b71d237bc70e8098eebd345e4a6997c5dad8885f
Denis Bulgakov <praeitor@gmail.com>
2025-04-12 13:57:52 +0400

    provider changes 3

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,9 +7,7 @@ terraform {
   required_version = ">= 0.13"
 
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+    endpoint                    = "https://storage.yandexcloud.net"
     bucket                      = "kittygram-tf-state"
     key                         = "terraform.tfstate"
     region                      = "ru-central1"
@@ -17,11 +15,12 @@ terraform {
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    force_path_style            = true
   }
 }
 
 provider "yandex" {
-  service_account_key_file = "./authorized_key.json"
+  service_account_key_file = "authorized_key.json"
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
