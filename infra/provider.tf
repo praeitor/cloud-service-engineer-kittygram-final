e93e7655cb200adf8470575eda3dc6909f1f6979
Denis Bulgakov <praeitor@gmail.com>
2025-04-12 16:32:56 +0400

    changes in providers (again)

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,45 +1,21 @@
-# terraform {
-#   required_providers {
-#     yandex = {
-#       source  = "yandex-cloud/yandex"
-#     }
-#   }
-#   required_version = ">= 0.13"
-
-#   backend "s3" {
-#     endpoint                    = "https://storage.yandexcloud.net"
-#     bucket                      = "kittygram-tf-state"
-#     key                         = "terraform.tfstate"
-#     region                      = "ru-central1"
-#     skip_region_validation      = true
-#     skip_credentials_validation = true
-#     skip_requesting_account_id  = true
-#     skip_s3_checksum            = true
-#     force_path_style            = true
-#   }
-# }
-
 terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.89.0"
     }
   }
+  required_version = ">= 0.13"
 
-  # S3-бакет в Object Storage (для хранения файла Terraform state)
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-    bucket = "kittygram-tf-state"
-    region = "ru-central1"
-    key    = "terraform.tfstate"
-
+    endpoint                    = "https://storage.yandexcloud.net"
+    bucket                      = "kittygram-tf-state"
+    key                         = "terraform.tfstate"
+    region                      = "ru-central1"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    force_path_style            = true
   }
 }
 
